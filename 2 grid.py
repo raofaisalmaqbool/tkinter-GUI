c9f4ee74b181ae8a7795a18c3e7386377a23be1b
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-04-14 10:59:19 +0500

    some more working with row and column

--- 2 grid.py
+++ 2 grid.py
@@ -4,8 +4,9 @@ root = Tk()
 
 # create a label widget
 myLabel1 = Label(root, text="Hello World!").grid(row=0, column=0)
-myLabel2 = Label(root, text="                     ").grid(row=1, column=1)
-myLabel3 = Label(root, text="My name is Faisal").grid(row=1, column=5)
+myLabel2 = Label(root, text="                     ").grid(row=0, column=1)
+myLabel3 = Label(root, text="My name is Faisal").grid(row=0, column=2)
+myLabel4 = Label(root, text="My name is Faisal").grid(row=1, column=1)
 
 # showing it into the secreen w.r.t  row and column
 # myLabel1.grid(row=0, column=0)
