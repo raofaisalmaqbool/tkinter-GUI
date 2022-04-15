7f1289b0c179b90529b828b0d74e7aa14ae1a423
raofaisalmaqbool <raofaisalmaqbool@gmail.com>
2022-04-15 15:36:13 +0500

    create frame in the window  like ,,, div in html ,,, and create two button

---
+++ 9 frams.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter.ttk import *
+# from PIL import ImageTk, Image
+
+root = Tk()
+root.title("Image")
+
+
+# show image on title side
+p1 = PhotoImage(file = 'img/logo.png')
+root.iconphoto(False, p1)
+ 
+
+frame = LabelFrame(root, text="this is window", ipadx=50, ipady=50)
+frame.pack(padx=50, pady=50)
+
+b1 = Button(frame, text="button 1") # button 1 show inside the frame
+b1.grid(row=0, column=0)                       
+
+b2 = Button(frame, text="button 2")  # buttion 2 show inside the frame
+b2.grid(row=1, column=1)
+
+root.mainloop()
